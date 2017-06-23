255e57b51593cff872e04c9990b3481bc95ff6e2
karan <karan.pathak@navyuginfo.com>
2017-06-23 18:10:15 +0530

    removed isDevelopingAddon hook from index.js

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-seo-meta-tags',
-  // For live reload when developing this addon, use the isDevelopingAddon hook.
-  isDevelopingAddon: function() {
-    return true;
-  }
+  name: 'ember-seo-meta-tags'
 };
